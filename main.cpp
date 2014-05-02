0e70c32c4f0fe5edb43170418817714f9f8d4370
pizzamanzoo <pizzamanzootoo@gmail.com>
2014-05-01 17:22:33 -0700

    Updated main, but not working completely yet

--- main.cpp
+++ main.cpp
@@ -2,26 +2,60 @@
 #include<string>
 
 
-void setNotes(std::string notes[17])
+void setNotes(std::string notes[17], int scale)
 {
-	notes[0] = "A";
-	notes[1] = "Bb";
-	notes[2] = "A#";
-	notes[3] = "B";
-	notes[4] = "C";
-	notes[5] = "C#";
-	notes[6] = "Db";
-	notes[7] = "D";
-	notes[8] = "D#";
-	notes[9] = "Eb";
-	notes[10] = "E";
-	notes[11] = "F";
-	notes[12] = "F#";
-	notes[13] = "Gb";
-	notes[14] = "G";
-	notes[15] = "G#";
-	notes[16] = "Ab";
+	
+	if(scale == 1)
+	{
+		notes[0] = "A";
+		notes[1] = "A#";
+		notes[2] = "B";
+		notes[3] = "C";
+		notes[4] = "C#";
+		notes[5] = "D";
+		notes[6] = "D#";
+		notes[7] = "E";
+		notes[8] = "F";
+		notes[9] = "F#";
+		notes[10] = "G";
+		notes[11] = "G#";
+	}
 
+	else if(scale == 2)
+	{
+		notes[0] = "Ab";
+	}
+	else if(scale == 0)
+	{
+
+	}
+}
+
+
+int checkInput(std::string in_note)
+{
+	if(in_note.find('#') != std::string::npos)
+	{
+		std::cout << "Sharp " << std::endl;
+		return 1;
+	}
+	else if(in_note.find('b') != std::string::npos)
+	{
+		std::cout << "Flat " << std::endl;
+		return 2;
+	}
+	else if(in_note.compare("F") == 0)
+	{
+		return 2;
+	}
+	else if(in_note.compare("C") == 0)
+	{
+		return 3;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 void getInput(std::string& in_note)
@@ -38,11 +72,15 @@ int main()
 	std::string in_note;
 	std::string notes[17];
 	int note_num;
-
-	setNotes(notes);
-
+	int scale;
+ 
+	
 	getInput(in_note);
 	std::cout << "NOTE: " << in_note << std::endl;
+
+	scale = checkInput(in_note);
+
+	setNotes(notes, scale);
 	for(int i = 0; i < 17; i++)
 	{
 		if(in_note.compare(notes[i]) == 0)
@@ -50,8 +88,9 @@ int main()
 			note_num = i;
 			i = 18;
 		}
-	} 
-	std::cout << notes[note_num + 10] << " is a fifth above " << in_note << std::endl;
+	}
+ 
+	std::cout << notes[note_num] << " is a fifth above " << in_note << std::endl;
 
 	
 	
