358216e64f17b593e09ea5520d919acf83f2dd18
pizzamanzoo <pizzamanzootoo@gmail.com>
2014-05-22 07:59:47 -0700

    Updated

--- main.cpp
+++ main.cpp
@@ -1,63 +1,25 @@
 #include<iostream>
 #include<string>
 
-
-void setNotes(std::string notes[17], int scale)
+void fillNoteArr(std::string notes[7], std::string temp_notes[7])
 {
-	
-	if(scale == 1)
+	for(int i = 0; i < 7; i++)
 	{
-		notes[0] = "A";
-		notes[1] = "A#";
-		notes[2] = "B";
-		notes[3] = "C";
-		notes[4] = "C#";
-		notes[5] = "D";
-		notes[6] = "D#";
-		notes[7] = "E";
-		notes[8] = "F";
-		notes[9] = "F#";
-		notes[10] = "G";
-		notes[11] = "G#";
+		notes[i] = temp_notes[i];
 	}
+}
 
-	else if(scale == 2)
+void setNotes(std::string notes[7], std::string in_note)
+{
+	if(in_note.compare("C") == 0)
 	{
-		notes[0] = "Ab";
-	}
-	else if(scale == 0)
-	{
-
+		std::string temp_notes[7] = {"C","D","E","F","G","A","B"};
+		fillNoteArr(notes, temp_notes);
 	}
 }
 
 
-int checkInput(std::string in_note)
-{
-	if(in_note.find('#') != std::string::npos)
-	{
-		std::cout << "Sharp " << std::endl;
-		return 1;
-	}
-	else if(in_note.find('b') != std::string::npos)
-	{
-		std::cout << "Flat " << std::endl;
-		return 2;
-	}
-	else if(in_note.compare("F") == 0)
-	{
-		return 2;
-	}
-	else if(in_note.compare("C") == 0)
-	{
-		return 3;
-	}
-	else
-	{
-		return 1;
-	}
-}
-
+		
 void getInput(std::string& in_note)
 {
 	std::cout << "Enter in your note: " << std::endl;
@@ -70,27 +32,20 @@ int main()
 {
 
 	std::string in_note;
-	std::string notes[17];
+	std::string notes[7];
 	int note_num;
 	int scale;
  
-	
+		
+
 	getInput(in_note);
 	std::cout << "NOTE: " << in_note << std::endl;
 
-	scale = checkInput(in_note);
+	 setNotes(notes, in_note);
 
-	setNotes(notes, scale);
-	for(int i = 0; i < 17; i++)
-	{
-		if(in_note.compare(notes[i]) == 0)
-		{
-			note_num = i;
-			i = 18;
-		}
-	}
+	
  
-	std::cout << notes[note_num] << " is a fifth above " << in_note << std::endl;
+	std::cout << notes[4] << " is a fifth above " << in_note << " in the key of " << in_note << std::endl;
 
 	
 	
